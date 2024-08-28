5b6fab047a606e80df45791ec1509e8a7da481ac
HeJin <HJ17352716597@163.COM>
2024-08-28 09:53:44 +0800

    todoList案例-1

---
+++ src_todoList/components/List.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="todo-main">
+    <Item v-for="todo of todoList" :key="todo.id"
+          :todo="todo"
+          :change-state="changeState"
+          :remove-item="removeItem"/>
+  </ul>
+</template>
+
+
+<script>
+import Item from "@/components/Item.vue";
+
+export default {
+  name: 'List',
+  components: {
+    Item
+  },
+  props:['todoList','changeState','removeItem'],
+  /*mounted() {
+    console.log(this.todoList)
+    console.log(this.changeState)
+  }*/
+}
+
+</script>
+
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
